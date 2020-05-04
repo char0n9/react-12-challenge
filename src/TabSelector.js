dab3105cdf9e190e9648e3693d9ab730fad2168d
char0n9 <59369140+char0n9@users.noreply.github.com>
2020-05-04 10:55:42 +0300

    solution

--- src/TabSelector.js
+++ src/TabSelector.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TabSelector extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      activeId: 'home'
-    };
-    this.handleChangeTab = this.handleChangeTab.bind(this);
-  }
-
-  handleChangeTab(event) {
-    const buttonId = event.target.id;
-    this.setState({ activeId: buttonId });
+    this.state = {};
   }
 
   render() {
-    const { activeId } = this.state;
     return (
       <div className="TabSelector">
         <button
           id="home"
-          onClick={this.handleChangeTab}
-          className={activeId === 'home' ? 'active' : ''}
+          onClick={this.props.func}
+          className={this.props.activeId === "home" ? "active" : ""}
         >
           Home
         </button>
         <button
           id="about"
-          onClick={this.handleChangeTab}
-          className={activeId === 'about' ? 'active' : ''}
+          onClick={this.props.func}
+          className={this.props.activeId === "about" ? "active" : ""}
         >
           About
         </button>
         <button
           id="contact"
-          onClick={this.handleChangeTab}
-          className={activeId === 'contact' ? 'active' : ''}
+          onClick={this.props.func}
+          className={this.props.activeId === "contact" ? "active" : ""}
         >
           Contact
         </button>
